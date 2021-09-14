cabc4307798e1364ac43ca15a58a5c0a56978d68
SaSaChu <teresa7397@gmail.com>
2021-09-14 17:54:19 +0800

    index大小版完成（輪播手機試成功真的是要哭出來）

--- js/public.js
+++ js/public.js
@@ -1,16 +1,27 @@
 $(function() {
 
 	var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 0,
+
     autoplay: {
       delay: 3000,
       disableOnInteraction: false
     },
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true
+    },
+
+    breakpoints: {
+      // when window width is >= 450px
+      450: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
     }
+
   });
 
   // 圖片縮放
@@ -24,5 +35,13 @@ $(function() {
     $('.dropdown-open').slideToggle();
   });
 
+  // 手機版Menu
+  $('.m_header_r').click(function() {
+    $('.m_menu').addClass('menu_open');
+  });
+
+  $('.menuClose').click(function() {
+    $('.m_menu').removeClass('menu_open');
+  });
 
 });
